4a4094d83828e6636170f7fe691bb5d9316747d8
Prageethek <146775637+Prageethek@users.noreply.github.com>
2025-04-25 22:56:50 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import Home from './src/Home'; // Make sure this path is correct
+import Home from './src/Home';
 
 export default function App() {
   return <Home />;
